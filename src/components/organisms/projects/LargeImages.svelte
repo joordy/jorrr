cf53746d854e3b7b7922f9e2e56f76e26e48158f
joordy <jmfronik@gmail.com>
2020-11-30 22:59:57 +0100

    Create static start for image slider on work page

--- src/components/organisms/projects/LargeImages.svelte
+++ src/components/organisms/projects/LargeImages.svelte
@@ -1,34 +1,47 @@
 <script>
   export let project;
+  console.log(project.largeImageSlider[0].src);
 </script>
 
 <style lang="scss">
-  .image__wrapper {
+  .image__caroussel {
     width: 80%;
     margin: 3rem auto 3rem auto;
-    display: flex;
-    overflow: hidden;
-    
+    overflow-x: scroll;
+    border: 5px solid hotpink;
     article {
+      display: flex;
+      width: 100%;
       height: 600px;
-      width: 90%;
-      margin: 0 auto;
-      background-color: hotpink;
       img {
         width: 100%;
         height: 100%;
-        object-fit: cover;
       }
     }
   }
 </style>
 
 <section>
-  <section class="image__wrapper">
-    {#each project.largeImageSlider as photo}
-      <article>
-        <img src={photo.src} alt={photo.alt}>
-      </article>
-    {/each} 
+  <section class="image__caroussel">
+    <article class="caroussel__slide">
+      <img
+        src="https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg"
+        alt="" />
+      <img
+        src="https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg"
+        alt="" />
+      <img
+        src="https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg"
+        alt="" />
+      <img
+        src="https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg"
+        alt="" />
+      <img
+        src="htts://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg"
+        alt="" />
+    </article>
+
+    <button id="backButton">back</button>
+    <button id="nextButton">next</button>
   </section>
 </section>
