589e330e61af4e4ea1180dd3e577048aecfa9b29
joordy <jmfronik@gmail.com>
2020-11-30 23:18:36 +0100

    Setup dynamic carousel

---
+++ src/components/molecules/projects/Carousel.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let images;
+  console.log(images);
+</script>
+
+<style lang="scss">
+  section {
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    // border: 5px solid hotpink;
+    display: flex;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    button {
+      position: absolute;
+      &:nth-of-type(1) {
+        left: 0;
+      }
+      &:nth-of-type(2) {
+        right: 0;
+      }
+    }
+  }
+</style>
+
+<section id="caroussel_cont">
+  {#each images as image}<img src={image.path} alt={image.alt} />{/each}
+  <button id="backBtn">Back</button>
+  <button id="nextBtn">Next</button>
+</section>
--- src/components/organisms/projects/LargeImages.svelte
+++ src/components/organisms/projects/LargeImages.svelte
@@ -1,28 +1,43 @@
 <script>
+  import Carousel from '@/components/molecules/projects/Carousel.svelte';
   export let project;
-  console.log(project.largeImageSlider[0].src);
+
+  const images = [
+    {
+      path: 'https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg',
+      alt: 'image1',
+      id: 1,
+    },
+    {
+      path: 'https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg',
+      alt: 'image2',
+      id: 2,
+    },
+    {
+      path: 'https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg',
+      alt: 'image3',
+      id: 3,
+    },
+    {
+      path: 'https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg',
+      alt: 'image4',
+      id: 4,
+    },
+  ];
 </script>
 
 <style lang="scss">
-  .image__caroussel {
+  section {
     width: 80%;
     margin: 3rem auto 3rem auto;
-    overflow-x: scroll;
-    border: 5px solid hotpink;
-    article {
-      display: flex;
-      width: 100%;
-      height: 600px;
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
+    height: 600px;
+    background-color: hotpink;
   }
 </style>
 
 <section>
-  <section class="image__caroussel">
+  <Carousel {images} />
+  <!-- <section class="image__caroussel">
     <article class="caroussel__slide">
       <img
         src="https://user-images.githubusercontent.com/48051912/100670369-0c86c280-335f-11eb-91de-cc8f52c1e73d.jpg"
@@ -43,5 +58,5 @@
 
     <button id="backButton">back</button>
     <button id="nextButton">next</button>
-  </section>
+  </section> -->
 </section>
